faf97a599d5a7a911fba99ea6666619330bc84ce
GUSTAVO LACERDA <gustavolacerda@GUSTAVOs-MacBook-Air.local>
2021-04-12 15:33:16 -0700

    login works

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,22 +2,44 @@ import React, { useState } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { MoveCollection } from '/imports/api/MoveCollection';
 import { Box } from './Box';
+import { LoginForm } from './LoginForm';
 
 //FRONT END
 
 export const App = () => {
+    const user = useTracker(() => Meteor.user());
     const moves = useTracker(() => MoveCollection.find({}).fetch()); // JS, using Meteor library
+    // update the box
     const increment = () => {
 	Meteor.call('moves.updateMove', moves[0].value + 1);
     };
+    const logout = () => Meteor.logout();
 
-    
     //ToDo: implement onClick elsewhere
     return(
-	<div>
-	    <h1>Welcome to Meteor!</h1>
-	    <ul onClick={() => increment()}>
+<div>
+        <>
+          {user ? (
+            <>
+              Hello {user.username}!
+              <div className="user" onClick={logout}><font color='blue'><u>log out</u></font></div>
+            </>
+          ) : (
+            <LoginForm />
+          )}
+        </>
+	    <h1>Welcome to Meteor!
+        </h1>
+        <ul onClick = {() => increment()} >
 		{ moves.map((move) => <Box key={ move._id } player={ move.value }/>) }
 	    </ul>
 	</div>
     )};
+
+// <ul onClick={() => increment()}>
+
+
+// Click
+// Button "stop timer", which also disables clicks
+// Display clicks per second field
+
--- server/main.js
+++ server/main.js
@@ -8,16 +8,30 @@ const insertMove = move => MoveCollection.insert( { _id: 1, value: move} );
 const updateMove = move => MoveCollection.update({}, {$set: { value: move}});
 
 
-Meteor.startup(() => {
-    if (MoveCollection.find().count() === 0) { // triple equal
-	insertMove(1);
-    }
-});
+// Meteor.startup(() => {    
+//     if (MoveCollection.find().count() === 0) { // triple equal
+// 	insertMove(1);
+//     }
+// });
 
 // API
 Meteor.methods({
     'moves.updateMove'(value) {updateMove(value)}
 })
 
+const SEED_USERNAME = 'gustavo';
+const SEED_PASSWORD = 'gustavo';
+
+Meteor.startup(() => {
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+});
 
 // Make a git commit from here
+
+// Publish everything
+// Tutorial: customize publishing, after user management
