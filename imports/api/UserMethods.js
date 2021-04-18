2a2b8ee20a4fa0f3b7c7a663be1a145ca08148b2
Jasper August Tootsi <jasperaugusttootsi@gmail.com>
2021-04-18 20:41:06 +0300

    add users fetching and displaying

--- imports/api/UserMethods.js
+++ imports/api/UserMethods.js
@@ -1,18 +1,20 @@
-import { check } from "meteor/check";
-import { Meteor } from 'meteor/meteor'
+import { Meteor } from "meteor/meteor";
 
 Meteor.methods({
-    "users.insert" (username, password) { //Does this register a function? The place that uses it passes a single 'user'
-        //  not users.  Is type conversion happening?
-      // What does this do? Is it mapping the function to each row in 'users'?
-      // For every user, if it doesn't exist, create it.
-      const userExists = Accounts.findUserByUsername(username);
-      if (!userExists) {
-        const userId = Accounts.createUser({username, password});
-      }
-    },
-    "users.list" () {
-      console.log(Meteor.users());
-      // return Accounts.find().fetch();
+  "users.insert"(username, password) {
+    //Does this register a function? The place that uses it passes a single 'user'
+    //  not users.  Is type conversion happening?
+    // What does this do? Is it mapping the function to each row in 'users'?
+    // For every user, if it doesn't exist, create it.
+    const userExists = Accounts.findUserByUsername(username);
+    if (!userExists) {
+      const userId = Accounts.createUser({ username, password });
     }
-  });
+  },
+  // if you remove "autopublish" package you need to go through the backend
+  // because then all of other user details aren't accessible to all users
+  "users.getAll"() {
+    const users = Meteor.users;
+    console.log(users);
+  },
+});
